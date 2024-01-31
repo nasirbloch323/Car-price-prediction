b1e1ae36d2bbdab84ecb441ee3a50e94b28ee709
Abdullah Khan Kakar <150433247+AbdullahProjects@users.noreply.github.com>
2024-02-01 02:25:31 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ brand_model = st.selectbox(f"Select a Model of {brand}:", np.sort(brand_df["Mode
 year = st.selectbox("Year of Purchase:", np.sort(df["Year"].unique()))
 # Km's drive
 # km_driven = st.selectbox("Kile-meters Driven:", df["KM's driven"].unique())
-km_driven = st.number_input("Kile-meter Driven:", min_value=1, max_value=500000)
+km_driven = st.number_input("Kilo-meter Driven:", min_value=10, max_value=500000)
 # Fuel
 fuel = st.selectbox("Fuel Type:", df["Fuel"].unique())
 # Car Documents
@@ -32,7 +32,7 @@ transmission = st.selectbox("Transmission:", df["Transmission"].unique())
 
 if st.button("Predict Price"):
     # create dataframe
-    predict_ = pd.DataFrame({
+    predictions = pd.DataFrame({
         "Make": [brand],
         "Model":[brand_model],
         "Year":[year],
@@ -47,15 +47,15 @@ if st.button("Predict Price"):
     # create new column of years range
     bins = [1999,2004,2008,2012,2016,2020,2024]
     labels = [1,2,3,4,5,6]
-    predict_["Year_Range"] = pd.cut(df["Year"], bins=bins, labels=labels)
+    predictions["Year_Range"] = pd.cut(df["Year"], bins=bins, labels=labels)
 
     # create new column of km driven range
     bins = [0,30000,60000,90000,120000,150000,180000,210000,224000,227000,300000,330000,360000,390000,410000,440000,470000,500000,533530]
     labels = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
-    predict_["KM's driven_Range"] = pd.cut(df["KM's driven"], bins=bins, labels=labels)
+    predictions["KM's driven_Range"] = pd.cut(df["KM's driven"], bins=bins, labels=labels)
     
     # Predict Price
-    predicted_price = loaded_model.predict(predict_)
+    predicted_price = loaded_model.predict(predictions)
 
     # Displaying the predicted price
     st.subheader(f"PKR Rs./ {int(predicted_price)}")
